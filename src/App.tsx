7333170b58caaa89b58e2d38d319a94be12248db
Dmitriy24S <devilins24@gmail.com>
2023-04-30 15:14:14 +0300

    feat: add React.lazy & Suspense in App for Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,45 @@
-import { useEffect } from 'react'
-import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import Navbar from './components/Navbar/Navbar'
 import ScrollToTop from './components/ScrollToTop'
 import ScrollToTopButton from './components/ScrollToTopButton/ScrollToTopButton'
-import AllProjectsPage from './pages/AllProjectsPage'
-import HomePage from './pages/HomePage'
+
+// import AllProjectsPage from './pages/AllProjectsPage'
+// import HomePage from './pages/HomePage'
+const AllProjectsPage = React.lazy(
+  () => import(/* webpackChunkName: "AllProjectsPage" */ './pages/AllProjectsPage')
+)
+const HomePage = React.lazy(
+  () => import(/* webpackChunkName: HomePage*/ './pages/HomePage')
+)
 
 function App() {
-  const fetchGithubInfo = async () => {
-    try {
-      const response = await fetch(`https://api.github.com/users/Dmitriy24S`)
-      // ! "API rate limit exceeded for 82.xxx.xxx.xxx. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)"
-      const data = await response.json()
-      console.log('fetchGithubInfo data', data)
-    } catch (error) {
-      console.log('fetchGithubInfo error:', error)
-      alert('fetchGithubInfo error')
-    }
-  }
-
-  useEffect(() => {
-    // fetchGithubInfo()
-    // const data = fetch('https://api.github.com/repos/Dmitriy24S/REPO')
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data))
-  }, [])
-
   return (
     <div className='App'>
+      {/* <BrowserRouter basename='/portfolio'> */}
       <BrowserRouter>
         <Navbar />
         <ScrollToTop />
         <ScrollToTopButton />
         <Routes>
           <Route path='/' element={<Navigate to='/portfolio' />} />
-          <Route path='/portfolio' element={<HomePage />} />
-          <Route path='/portfolio/projects' element={<AllProjectsPage />} />
+          <Route
+            path='/portfolio'
+            element={
+              <Suspense>
+                <HomePage />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/portfolio/projects'
+            element={
+              <Suspense>
+                <AllProjectsPage />
+              </Suspense>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
