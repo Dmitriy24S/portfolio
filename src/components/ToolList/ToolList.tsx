6a84baf9f101837ce065687f2ce71b11cd1e2ef0
Dmitriy24S <devilins24@gmail.com>
2023-04-30 20:26:27 +0300

    fix: ToolList img baseurl bug

--- src/components/ToolList/ToolList.tsx
+++ src/components/ToolList/ToolList.tsx
@@ -1,57 +1,71 @@
+import css3 from '../../assets/tools/css3-logo.svg'
+import framerMotion from '../../assets/tools/framer-motion-logo.svg'
+import git from '../../assets/tools/git-logo.svg'
+import github from '../../assets/tools/github-logo.svg'
+import mui from '../../assets/tools/mui-logo.svg'
+import node from '../../assets/tools/node-logo.svg'
+import react from '../../assets/tools/react-logo.svg'
+import reactQuery from '../../assets/tools/react-query-logo.svg'
+import redux from '../../assets/tools/redux-logo.svg'
+import rtl from '../../assets/tools/rtl-logo.svg'
+import sass from '../../assets/tools/sass-logo.svg'
+import tailwind from '../../assets/tools/tailwind-logo.svg'
+import typescript from '../../assets/tools/typescript-logo.svg'
+
 import styles from './ToolList.module.scss'
 
 const tools = [
   {
     name: 'React',
-    iconName: 'react',
+    icon: react,
   },
   {
     name: 'TypeScript',
-    iconName: 'typescript',
+    icon: typescript,
   },
   {
     name: 'Redux',
-    iconName: 'redux',
+    icon: redux,
   },
   {
     name: 'React Query',
-    iconName: 'react-query',
+    icon: reactQuery,
   },
   {
     name: 'Node.js',
-    iconName: 'node',
+    icon: node,
   },
   {
     name: 'CSS',
-    iconName: 'css3',
+    icon: css3,
   },
   {
     name: 'Sass',
-    iconName: 'sass',
+    icon: sass,
   },
   {
     name: 'Tailwind',
-    iconName: 'tailwind',
+    icon: tailwind,
   },
   {
     name: 'MUI',
-    iconName: 'mui',
+    icon: mui,
   },
   {
     name: 'Framer Motion',
-    iconName: 'framer-motion',
+    icon: framerMotion,
   },
   {
     name: 'Testing Library',
-    iconName: 'rtl',
+    icon: rtl,
   },
   {
     name: 'Git',
-    iconName: 'git',
+    icon: git,
   },
   {
     name: 'GitHub',
-    iconName: 'github',
+    icon: github,
   },
 ]
 
@@ -60,7 +74,16 @@ const ToolList = () => {
     <div className={styles.toolList}>
       {tools.map((tool) => (
         <div className={styles.tool} key={tool.name}>
-          <img src={`assets/tools/${tool.iconName}-logo.svg`} alt='' />
+          <img
+            src={
+              // `/assets/tools/${tool.iconName}-logo.svg`
+              // `./assets/tools/${tool.iconName}-logo.svg`
+              // `portfolio/assets/tools/${tool.iconName}-logo.svg`
+              // `./src/assets/tools/${tool.iconName}-logo.svg`
+              tool.icon
+            }
+            alt=''
+          />
           <span>{tool.name}</span>
         </div>
       ))}
